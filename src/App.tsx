ccebe693523a85f3a2fd7103139a5817c1649399
Faizel Quabili <faizelq@udel.edu>
2022-02-08 15:24:22 -0500

    Task 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript by Faizel Quabili
+                UD CISC275 with React Hooks and TypeScript by Task 1: Faizel
+                Quabili, Task 2: Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
