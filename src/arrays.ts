171e1804fd9daf8a6bf1c93b3d4b6725abf04dc1
Faizel Quabili <faizelq@udel.edu>
2022-02-20 18:10:26 -0500

    countShortWords function

--- src/arrays.ts
+++ src/arrays.ts
@@ -76,7 +76,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const lessThanFour = words.filter((str: string): boolean => str.length < 4);
+    return lessThanFour.length;
 }
 
 /**
