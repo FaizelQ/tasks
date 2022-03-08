9ad11ea6de3b7ea5e93cf5f9d0c6d9f16a313329
Faizel Quabili <faizelq@udel.edu>
2022-03-07 19:37:20 -0500

    completed task 8 with no linter errors

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,56 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+//🤡, 🦃, 🎄, 🐐, 🐇
+//Halloween, Thanksgiving, Christmas, Eid, Easter
+//Alphabetical
+//Christmas, Easter, Eid, Halloween, Thanksgiving
+//By Date
+//Easter, Eid, Halloween, Thanksgiving, Christmas
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holidayState, setHoliday] = useState<string>("🤡");
+
+    const alphaRecord: Record<string, string> = {
+        "🤡": "🦃",
+        "🦃": "🎄",
+        "🎄": "🐐",
+        "🐐": "🐇",
+        "🐇": "🤡"
+    };
+
+    const byDateRecord: Record<string, string> = {
+        "🐇": "🐐",
+        "🐐": "🤡",
+        "🤡": "🦃",
+        "🦃": "🎄",
+        "🎄": "🐇"
+    };
+
+    return (
+        <div>
+            <p>Holiday: {holidayState}</p>
+            <div>
+                <Button
+                    onClick={() =>
+                        setHoliday(
+                            (currentHoliday) => alphaRecord[currentHoliday]
+                        )
+                    }
+                >
+                    Advance by Alphabet
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={() =>
+                        setHoliday(
+                            (currentHoliday) => byDateRecord[currentHoliday]
+                        )
+                    }
+                >
+                    Advance by Year
+                </Button>
+            </div>
+        </div>
+    );
 }
