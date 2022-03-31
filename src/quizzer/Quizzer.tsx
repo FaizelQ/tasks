82c25cf6c6caceee9bcfde999b79c3738fe1be77
Faizel Quabili <faizelq@udel.edu>
2022-03-30 21:08:25 -0400

    started quizview test

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { Quiz } from "../interfaces/quiz";
-import QUIZDATA from "../data/quizzes.json";
 import { QuizView } from "./QuizView";
 export default React.memo(Quizzer);
 export function Quizzer(): JSX.Element {
